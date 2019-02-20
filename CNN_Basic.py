adf2ef8fd01a106079d66b1e7d929439fbd81ef4
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-02-20 07:02:02 +0100

    feat: dataloader integration
    Dataloader has been created in order to automatize the files load.

--- CNN_Basic.py
+++ CNN_Basic.py
@@ -1,6 +1,12 @@
 #%% Import and function declaration
 # 0| Load
-from data_process import PlantDataset
+from data_process import PlantDataset, Rescale, RandomCrop, ToTensor
+
+from torchvision import transforms
+from torch.utils.data import DataLoader
 
 #%% Load dataset
-train_data = PlantDataset(data_path='data/train')
+train_data = PlantDataset(data_path='data/train',
+                          transform=transforms.Compose([Rescale(135), RandomCrop(224), ToTensor()]))
+
+dataloader = DataLoader(train_data, batch_size=4, shuffle=True, num_workers=4)
