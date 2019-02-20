b8bb85cfcbb6d3dff95863713646210025d33573
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-02-20 08:50:22 +0100

    feat: create Image Visualizer
    Images Visualizer:
    Allows to visualize the images that are loaded into the CNN

---
+++ utils.py
@@ -0,0 +1,32 @@
+"""
+Image visualizer:
+Allows to see the images the "loader" functions are providing to the CNN
+"""
+
+import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
+
+class_names = 'train_dataset'.classes
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+# Get a batch of training data
+inputs, classes = next(iter('train_loader'))
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+
+imshow(out, title=[class_names[x] for x in classes])
+for element in classes:
+    print(class_names[element])
